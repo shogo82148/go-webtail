2dba07dc01ff2a5b9ffde69af620a9a7f1fd9aff
Ichinose Shogo <shogo82148@gmail.com>
2015-06-21 17:21:10 +0900

    system messageを追加。ファイルのtailも実装。

--- cmd/webtail/main.go
+++ cmd/webtail/main.go
@@ -1,40 +1,54 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
 	"os"
+	"path"
 
 	"github.com/shogo82148/go-webtail"
 	"golang.org/x/net/websocket"
 )
 
-func IndexHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprint(w, `<!DOCTYPE html>
+func IndexHandler(prefix string) func(w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, `<!DOCTYPE html>
 <html>
   <head>
-    <title>websocket</title>
+    <title>go-webtail</title>
+    <style type="text/css">
+    .system-message { color: lightsteelblue; }
+    </style>
   </head>
 
   <body>
-    <samp id="lines"></sampo>
+    <pre id="lines"></pre>
     <script type="text/javascript">
 (function(){
     function connect(uri) {
         var socket = new WebSocket(uri);
         var elem = document.getElementById("lines");
         socket.addEventListener("open", function (e) {
-            console.log("open websocket");
-            elem.innerHTML = "";
+            var line = document.createElement("div");
+            line.className = "system-message";
+            line.innerText = "connection connected";
+            elem.appendChild(line);
         });
 
         socket.addEventListener("close", function (e) {
-            console.log("close websocket");
+            var line = document.createElement("div");
+            line.className = "system-message";
+            line.innerText = "connection closed";
+            elem.appendChild(line);
             setTimeout(reconnect, 1000);
         });
 
         socket.addEventListener("error", function (e) {
-            console.log("error!!", e);
+            var line = document.createElement("div");
+            line.className = "system-message";
+            line.innerText = "connection error";
+            elem.appendChild(line);
         });
 
         socket.addEventListener("message", function (e) {
@@ -44,7 +58,7 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
             elem.appendChild(line);
         });
     }
-    var uri = "ws://" + location.host + "/follow";
+    var uri = "ws://" + location.host + "%s/follow";
     connect(uri);
     function reconnect() {
         connect(uri);
@@ -53,16 +67,44 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
     </script>
   </body>
 </html>
-`)
+`, prefix)
+	}
 }
 
 func main() {
-	t, _ := webtail.NewTailReader(os.Stdin)
+	var port int
+	var prefix string
 
-	http.Handle("/follow", websocket.Handler(t.FollowHandler))
-	http.HandleFunc("/", IndexHandler)
+	flag.IntVar(&port, "port", 8080, "listen port(default: 8080)")
+	flag.IntVar(&port, "p", 8080, "listen port")
+	flag.StringVar(&prefix, "prefix", "/", "prefix of url")
+	flag.Parse()
 
-	err := http.ListenAndServe(":8080", nil)
+	if len(prefix) < 1 || prefix[0] != '/' {
+		prefix = "/" + prefix
+	}
+
+	args := flag.Args()
+	if len(args) == 0 {
+		args = []string{"-"}
+	}
+
+	for _, file := range args {
+		if file == "-" {
+			// tail stdin
+			t, _ := webtail.NewTailReader(os.Stdin)
+			http.Handle(prefix+"/follow", http.StripPrefix(prefix, websocket.Handler(t.FollowHandler)))
+			http.HandleFunc(prefix+"/", IndexHandler(prefix))
+		} else {
+			// tail file
+			t, _ := webtail.NewTailFile(file)
+			basename := path.Base(file)
+			http.Handle(prefix+"/"+basename+"/follow", http.StripPrefix(prefix+"/"+basename, websocket.Handler(t.FollowHandler)))
+			http.HandleFunc(prefix+"/"+basename, IndexHandler(prefix+"/"+basename))
+		}
+	}
+
+	err := http.ListenAndServe(fmt.Sprintf(":%d", port), nil)
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
 	}
