d74282b8d09a24b10142234db45c2c85c7108cc0
Ichinose Shogo <shogo82148@gmail.com>
2015-07-19 11:32:43 +0900

    HTMLファイルを別ファイルに分離

--- cmd/webtail/main.go
+++ cmd/webtail/main.go
@@ -13,75 +13,7 @@ import (
 
 func IndexHandler(prefix string) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, `<!DOCTYPE html>
-<html>
-  <head>
-    <title>go-webtail</title>
-    <style type="text/css">
-    .system-message { color: lightsteelblue; }
-    </style>
-  </head>
-
-  <body>
-    <pre id="lines"></pre>
-    <script type="text/javascript">
-(function(){
-    var reconnecting = true;
-    var socket;
-    function connect(uri) {
-        socket = new WebSocket(uri);
-        var elem = document.getElementById("lines");
-        socket.addEventListener("open", function (e) {
-            var line = document.createElement("div");
-            line.className = "system-message";
-            line.innerText = "connection connected";
-            elem.appendChild(line);
-            reconnecting = false;
-        });
-
-        socket.addEventListener("close", function (e) {
-            socket = undefined;
-            if (!reconnecting) {
-                var line = document.createElement("div");
-                line.className = "system-message";
-                line.innerText = "connection closed";
-                elem.appendChild(line);
-            }
-            reconnecting = true;
-            setTimeout(reconnect, 10000);
-        });
-
-        socket.addEventListener("error", function (e) {
-            if (!reconnecting) {
-                var line = document.createElement("div");
-                line.className = "system-message";
-                line.innerText = "connection error";
-                elem.appendChild(line);
-            }
-        });
-
-        socket.addEventListener("message", function (e) {
-            var data = JSON.parse(e.data);
-            var line = document.createElement("div");
-            line.innerText = data.text;
-            elem.appendChild(line);
-        });
-    }
-    var uri = "ws://" + location.host + "%s/follow";
-    connect(uri);
-    function reconnect() {
-        connect(uri);
-    }
-
-    setInterval(function() {
-        if (socket) socket.send('ping');
-    }, 20*1000);
-
-})();
-    </script>
-  </body>
-</html>
-`, prefix)
+		fmt.Fprintf(w, indexHTML, prefix)
 	}
 }
 
